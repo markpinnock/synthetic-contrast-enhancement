45d4f2d4c3a980dc4fcc0b139b7b9b3f2a4dfeb5
Mark Pinnock <rmappin@ucl.ac.uk>
2023-01-24 17:25:14 +0000

    Add discriminator and unit tests

---
+++ multi-phase-sce/src/multi_phase/networks/components/discriminator.py
@@ -0,0 +1,86 @@
+import numpy as np
+import tensorflow as tf
+
+from .layer.layers import DownBlock
+
+
+class Discriminator(tf.keras.Model):
+
+    """ PatchGAN discriminator for Pix2pix and CycleGAN
+        :param initialiser: e.g. keras.initializers.RandomNormal
+        :param config: configuration dict
+     """
+
+    def __init__(
+        self,
+        initialiser: tf.keras.initializers.Initializer,
+        config: dict,
+        name: str | None = None
+    ):
+        super().__init__(name=name)
+    
+        # Check network and image dimensions
+        img_dims = config["img_dims"]
+        assert len(img_dims) == 3, "3D input only"
+        max_num_layers = int(
+            np.log2(np.min([img_dims[0], img_dims[1]]) / 4)
+        )
+        max_z_downsample = int(np.floor(np.log2(img_dims[2])))
+        nc = config["ndf"] # Number first layer channels
+        num_layers = config["d_layers"]
+
+        # Get layers incorporating phase information
+        self.phase_layers = config["d_phase_layers"]
+       
+        assert num_layers <= max_num_layers and num_layers >= 0, \
+            f"Maximum number of discriminator layers: {max_num_layers}"
+        self.encoder = []      
+
+        # PatchGAN i.e. NxN receptive field for N > 1
+        batch_norm = False
+
+        for i in range(0, num_layers):
+            if i > 0: batch_norm = True
+            channels = tf.minimum(nc * 2 ** i, 512)
+
+            if i > max_z_downsample:
+                strides = (2, 2, 1)
+                kernel = (4, 4, 2)
+            else:
+                strides = (2, 2, 2)
+                kernel = (4, 4, 4)
+            
+            self.encoder.append(
+                DownBlock(
+                    channels,
+                    kernel,
+                    strides,
+                    initialiser=initialiser,
+                    model="discriminator",
+                    batch_norm=batch_norm, name=f"down_{i}"
+                )
+            )
+        
+        self.encoder.append(tf.keras.layers.Conv3D(
+            1, (4, 4, 1), (1, 1, 1),
+            padding="valid",
+            kernel_initializer=initialiser, name="output"))
+
+        layer_names = [layer.name for layer in self.encoder]
+
+        for phase_input in self.phase_layers:
+            assert phase_input in layer_names, (phase_input, layer_names)
+
+    def call(self, x: tf.Tensor, phase: tf.Tensor = None) -> tf.Tensor:
+        """ Discriminator call method
+            :param x: input image volume
+            :param phase: phase information
+        """
+
+        for conv in self.encoder:
+            if conv.name in self.phase_layers:
+                x = conv(x, phase, training=True)
+            else:
+                x = conv(x, training=True)
+
+        return x
---
+++ test_multi_phase/networks/components/test_discriminator.py
@@ -0,0 +1,121 @@
+import numpy as np
+import pytest
+import tensorflow as tf
+
+from multi_phase.networks.components.discriminator import Discriminator
+
+
+#-------------------------------------------------------------------------
+
+@pytest.mark.parametrize(
+    "depth,img_dims",
+    [
+        (3, [2, 16, 16, 8, 1]),
+        (4, [4, 32, 32, 16, 1]),
+        (5, [4, 64, 64, 16, 1])
+    ]
+)
+def test_ImgDimsAssert(depth: int, img_dims: list[int]) -> None:
+    """ Test assertion raised if too many layers """
+
+    config = {
+        "ndf": 4,
+        "d_layers": depth,
+        "img_dims": img_dims[1:-1],
+        "d_phase_layers": []
+    }
+
+    init = tf.keras.initializers.Zeros()
+
+    with pytest.raises(AssertionError):
+        _ = Discriminator(init, config)
+
+
+#-------------------------------------------------------------------------
+
+@pytest.mark.parametrize(
+    "depth,img_dims,out_dims",
+    [
+        (1, [1, 16, 16, 16, 1], (1, 5, 5, 8, 1)),
+        (2, [2, 16, 16, 8, 1], (2, 1, 1, 2, 1)),
+        (2, [4, 32, 32, 16, 1], (4, 5, 5, 4, 1)),
+        (2, [4, 64, 64, 16, 1], (4, 13, 13, 4, 1)),
+        (3, [4, 64, 64, 16, 1], (4, 5, 5, 2, 1))
+    ]
+)
+def test_NoPhaseOutput(
+    depth: int,
+    img_dims: list[int],
+    out_dims: tuple[int]
+) -> None:
+    """ Test discriminator output is correct size """
+
+    config = {
+        "ndf": 4,
+        "d_layers": depth,
+        "img_dims": img_dims[1:-1],
+        "d_phase_layers": []
+    }
+
+    init = tf.keras.initializers.Zeros()
+
+    model = Discriminator(init, config)
+    img = tf.zeros(img_dims)
+    out = model(img)
+
+    assert out.shape == out_dims
+
+
+#-------------------------------------------------------------------------
+
+@pytest.mark.parametrize(
+    "phase_layers", [(["down_3"]), (["up_0", "down_1"])]
+)
+def test_PhaseLayerAssert(phase_layers: list[int]) -> None:
+    """ Test phase layers checked for correctly """
+
+    img_dims = [4, 64, 64, 16, 1]
+    out_dims = (4, 5, 5, 2, 1)
+    config = {
+        "ndf": 4,
+        "d_layers": 3,
+        "img_dims": img_dims[1:-1],
+        "d_phase_layers": phase_layers
+    }
+
+    init = tf.keras.initializers.Zeros()
+
+    with pytest.raises(AssertionError):
+        _ = Discriminator(init, config)
+
+
+#-------------------------------------------------------------------------
+
+@pytest.mark.parametrize(
+    "phase_layers",
+    [
+        (["down_0"]),
+        (["down_0", "down_1"]),
+        (["down_0", "down_2"]),
+        (["down_0", "down_1", "down_2"]),
+    ]
+)
+def test_PhaseOutput(phase_layers: list[int]) -> None:
+    """ Test discriminator output is correct size """
+
+    img_dims = [4, 64, 64, 16, 1]
+    out_dims = (4, 5, 5, 2, 1)
+    config = {
+        "ndf": 4,
+        "d_layers": 3,
+        "img_dims": img_dims[1:-1],
+        "d_phase_layers": phase_layers
+    }
+
+    init = tf.keras.initializers.Zeros()
+
+    model = Discriminator(init, config)
+    img = tf.zeros(img_dims)
+    out = model(img, tf.constant([1.0, 2.0]))
+
+    assert out.shape == out_dims
